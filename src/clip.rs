852e0a77c5defb1a06560b6ac0edaac683eebfae
Javier Arrieta <javier.arrieta@zalando.ie>
2020-02-07 22:02:53 +0000

    Create mosaic

--- src/clip.rs
+++ src/clip.rs
@@ -1,9 +1,9 @@
 extern crate chrono;
 
-use std::path::PathBuf;
+use std::path::{PathBuf, Path};
 use self::chrono::{NaiveDateTime, Utc};
 use crate::camera::{CameraFile, Camera};
-use std::fs::{DirEntry, File, rename};
+use std::fs::{DirEntry, File, remove_file};
 use std::io;
 
 use crate::formats::{parse_tesla_timestamp, parse_error_to_io_error, file_stem, err_from_str};
@@ -29,9 +29,7 @@ impl SentryClip {
     }
 
     pub fn files_per_camera(&self, camera: &Camera) -> Vec<&CameraFile> {
-        let mut res = self.clips.iter().filter(|f| f.camera.eq(camera)).collect::<Vec<&CameraFile>>();
-        res.sort_by(|a,b| a.start_time.cmp(&b.start_time));
-        res
+        self.clips.iter().filter(|f| f.camera.eq(camera)).collect::<Vec<&CameraFile>>()
     }
 
     pub fn concatenate_camera_files(&self, camera: &Camera) -> io::Result<String> {
@@ -53,6 +51,54 @@ impl SentryClip {
         Ok(result_tmp_file.to_string())
     }
 
+    pub fn create_mosaic(&self, file_cameras: &Vec<(String, Camera)>) -> io::Result<()> {
+
+        let mosaic_file = self.mosaic_file()?;
+        log::info!("Composing mosaic clip '{}'", mosaic_file.display());
+
+        let filter_params = format!(
+            "nullsrc=size=1280x960 [base]; [0:v] setpts=PTS-STARTPTS, scale=640x480 [upperleft]; [1:v] setpts=PTS-STARTPTS, scale=640x480 [upperright]; \
+            [2:v] setpts=PTS-STARTPTS, scale=640x480 [lowerleft]; [3:v] setpts=PTS-STARTPTS, scale=640x480 [lowerright]; [base][upperleft] overlay=shortest=1 [tmp1]; \
+            [tmp1][upperright] overlay=shortest=1:x=640 [tmp2]; [tmp2][lowerleft] overlay=shortest=1:y=480 [tmp3]; [tmp3][lowerright] overlay=shortest=1:x=640:y=480, \
+            drawtext=text='%{{pts\\:gmtime\\:{}\\:%d-%m-%Y %T}}': x=100 : y=800 : box=0: fontsize=32: fontcolor=GoldenRod",
+            self.clips[0].start_time.timestamp()
+        );
+        let mut args = vec![
+            "-filter_complex",
+            filter_params.as_str()
+        ];
+
+        for f in file_cameras {
+            args.push("-i");
+            args.push(f.0.as_str());
+        };
+        args.push("-c:v");
+        args.push("libx264");
+        args.push(mosaic_file.to_str().ok_or(err_from_str("Cannot get path for mosaic file path"))?);
+
+        Command::new("ffmpeg")
+            .args(args)
+            .status()?;
+
+        delete_files(file_cameras.iter().map(|t| t.0.clone()).collect())?;
+
+        Ok(())
+    }
+
+    pub fn mosaic_file(&self) -> io::Result<PathBuf> {
+        let mosaic_filename = format!("{}-mosaic.mp4", self.when.format("%Y-%m-%d_%H-%M-%S"));
+        Ok(self.folder.parent().ok_or(err_from_str(format!("Cannot find parent folder of {}", self.folder.display()).as_str()))?
+            .join(mosaic_filename.as_str()))
+
+    }
+}
+
+fn delete_files(files: Vec<String>) -> io::Result<()> {
+    for file in files {
+        let path = Path::new(&file);
+        remove_file(path)?;
+    }
+    Ok(())
 }
 
 fn create_temp_playlist(files: &Vec<&CameraFile>, playlist_filename: &str) -> io::Result<()> {
@@ -66,12 +112,13 @@ fn create_temp_playlist(files: &Vec<&CameraFile>, playlist_filename: &str) -> io
 }
 
 fn process_folder(root: &DirEntry) -> io::Result<Vec<CameraFile>> {
-    let clips: Vec<CameraFile> = list_files(root)?.into_iter().filter_map(|e| {
+    let mut clips: Vec<CameraFile> = list_files(root)?.into_iter().filter_map(|e| {
         match CameraFile::from(e.path().as_path()) {
             Ok(f) => { Some(f) },
             Err(err) => { log::error!("Found error processing clip {}: {}", &e.path().display(), err); None },
         }
     }).collect();
+    clips.sort_by(|a,b| a.start_time.cmp(&b.start_time));
     log::debug!("Processed {} sentry clips", &clips.len());
     Ok(clips)
 }
--- src/main.rs
+++ src/main.rs
@@ -20,23 +20,19 @@ fn main() -> io::Result<()> {
     let clips_folders = children_folders(sentryclips_root)?;
     let clips: Vec<SentryClip> = clips_folders.into_iter().filter_map(|e|SentryClip::from_folder(&e).ok()).collect();
 
-    let clips_and_cameras: Vec<(SentryClip, Vec<(String, Camera)>)> = clips.into_iter().filter_map(|clip| {
+    let _clips_and_cameras: Vec<(SentryClip, Vec<(String, Camera)>)> = clips.into_iter().filter_map(|clip| {
         if !clip.is_empty() {
             log::info!("Processing clip folder {} [{}]", &clip.folder.display(), &clip.when);
             let all_cameras = Camera::all_cameras().into_iter();
             let clip_files_and_camera: Vec<(String, Camera)> = all_cameras.filter_map(|camera| {
                 clip.concatenate_camera_files(&camera).ok().map(|f| (f, camera))
             }).collect();
+            //Create mosaic
+            clip.create_mosaic(&clip_files_and_camera)
+                .map_err(|e| log::error!("Error creating mosaic for clip {}: {}", clip.folder.display(), e)).unwrap();
             Some((clip, clip_files_and_camera))
         } else { None}
     }).collect();
-//    let camera_files_per_clip: Vec<(DirEntry, Vec<CameraFile>)> =
-//        clips_folders.into_iter().filter_map(|e| process_folder(&e).ok().map(|f| (e, f))).collect();
-//
-//    camera_files_per_clip.into_iter().for_each(|t| {
-//        log::debug!("{} [{}]", t.0.path().as_path().display(), t.1.len());
-//        t.1.into_iter().for_each(|f| println!("\t{}", &f));
-//    });
 
     Ok(())
 }
